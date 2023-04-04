c3988505d444835232535798acff42125a179234
Sandeep-morya <saabmaurya@gmail.com>
2023-04-04 11:41:42 +0530

    routes added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 ﻿const express = require("express");
 const cors = require("cors");
+const userRoute = require("./routes/users");
 const app = express();
+
 // :: middlewares ::
 app.use(cors());
 app.use(express.json());
+
 // :: routes ::
 app.get("/", (req, res) => {
 	res.send({ message: "Server is runnig" });
 });
+
+app.use("/users", userRoute);
 
 app.listen(8080, () => {
 	console.log("server is running");
---
+++ routes/users.js
@@ -0,0 +1,27 @@
+﻿const { Router } = require("express");
+const handler = require("express-async-handler");
+
+const router = Router();
+
+router.get(
+	"/",
+	handler(async (req, res) => {
+		res.send({ message: "user get Route is working" });
+	}),
+);
+
+router.post(
+	"/register",
+	handler(async (req, res) => {
+		res.send({ message: "user register Route is working" });
+	}),
+);
+
+router.post(
+	"/login",
+	handler(async (req, res) => {
+		res.send({ message: "user login Route is working" });
+	}),
+);
+
+module.exports = router;
