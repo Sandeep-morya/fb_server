9d0265e934f2082b250598f711e5aefe0273612a
Sandeep-morya <saabmaurya@gmail.com>
2023-04-04 11:16:36 +0530

    server iniated

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const express = require("express");
+const cors = require("cors");
+const app = express();
+// :: middlewares ::
+app.use(cors());
+app.use(express.json());
+// :: routes ::
+app.get("/", (req, res) => {
+	res.send({ message: "Server is runnig" });
+});
+
+app.listen(8080, () => {
+	console.log("server is running");
+});
