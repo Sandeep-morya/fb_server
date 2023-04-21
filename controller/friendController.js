7fa3e684c8db7153da535612e053bf8122ce5294
Sandeep-morya <saabmaurya@gmail.com>
2023-04-21 11:47:15 +0530

    server start script changed

--- controller/friendController.js
+++ controller/friendController.js
@@ -12,14 +12,15 @@ const addFriend = asyncHandler(async (req, res) => {
 
 const acceptRequest = asyncHandler(async (req, res) => {
 	const { user_id, friendRequest, users } = req.body;
-
+	// :: add friend in list of friends of the request sender ::
 	await User.findByIdAndUpdate(friendRequest, {
 		$push: { friends: user_id },
 	});
+	// :: add friend in list of friends of the request accepter ::
 	await User.findByIdAndUpdate(user_id, {
 		$push: { friends: friendRequest },
 	});
-
+	// :: removing request from list of friends request of user ::
 	await User.findByIdAndUpdate(user_id, {
 		$pull: { requests: friendRequest },
 	});
